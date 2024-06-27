dbd0a58455721c5859c003fa31086b5600bd776d
onurbeht <brunnobatista25@gmail.com>
2024-06-27 15:46:15 -0300

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+function App() {
+  return (
+    <>
+      <main className="">
+        <div>Logo</div>
+        <section>
+          Entrada do texto
+          <p>Apenas letras Minusculas e sem acento</p>
+        </section>
+        <section>BOTAO - Criptografar BOTAO - Descriptografar</section>
+        <section>
+          Criptografia do texto
+          <p>BOTAO - COPIAR</p>
+        </section>
+      </main>
+    </>
+  );
+}
+
+export default App;
